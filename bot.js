45a0bdfea509d25eb8d1f3e63bb273f4d1aa44d2
Hamza <hmmmza78@gmail.com>
2022-06-26 14:03:38 +0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+const Telegraf = require("telegraf");
+
+const bot = new Telegraf("5175387310:AAFHQXS8BryfPeI7gZGL_r00Dkdh2gSsLfg");
+
+// bot.start((ctx) => {
+//     ctx.reply("Hello bot dev! :)");
+// })
+
+bot.launch();
